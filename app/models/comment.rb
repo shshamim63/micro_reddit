6a4323d92e91390d108f28586d3ab24bb8b35556
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-24 22:13:21 +0600

    added validating and association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   validates :comment, presence: true, length: { maximum: 255 }
   validates :user_id, presence: true
   validates :post_id, presence: true
+  belongs_to :user, :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 90 }
   validates :body, presence: true, length: { maximum: 255 }
   validates :user_id, presence: true
+  has_many :comments, dependent: :destroy
+  belongs_to :user 
 end
