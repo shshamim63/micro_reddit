3f2e80659afd4db3f9a7135d441df97a48249e56
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-05-24 22:07:42 +0600

    added models validation and migration file

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  validates :comment, presence: true, length: { maximum: 255 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 90 }
+  validates :body, presence: true, length: { maximum: 255 }
+  validates :user_id, presence: true
 end
